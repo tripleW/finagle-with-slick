dc3612984c8ffe41dda7a8d5f4461e669476b9fe
letusfly85 <letusfly95@gmail.com>
2017-04-04 13:25:57 +0900

    add argonaut for finagle rest api sample

--- build.sbt
+++ build.sbt
@@ -12,6 +12,10 @@ libraryDependencies ++= {
   Seq(
     "com.typesafe.slick" % "slick_2.11" % "3.2.0",
     "com.twitter" %% "finagle-http" % "6.43.0",
+    "com.github.finagle" %% "finch-core" % "0.14.0",
+    //"com.github.finagle" %% "finch-json4s" % "0.14.0",
+    "com.github.finagle"  %% "finch-argonaut"  % "0.14.0", 
+    "io.argonaut" % "argonaut_2.11" % "6.1",
     "mysql" % "mysql-connector-java" % "5.1.28"
   )
 }
--- src/main/scala/io/triplew/example/Server.scala
+++ src/main/scala/io/triplew/example/Server.scala
@@ -1,17 +1,74 @@
 package io.triplew.example
 
+import argonaut.Argonaut._
+import argonaut.CodecJson
+import io.finch._
 import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http
 import com.twitter.util.{Await, Future}
 
+import scala.concurrent.{Future => SFuture, Await => SAwait}
+import scala.concurrent.duration.Duration
+
+import com.typesafe.config.ConfigFactory
+import slick.driver.MySQLDriver.api._
+
+import io.finch._
+import io.finch.argonaut._
+import scala.util.{Failure, Success, Try}
+
+case class Device(id: Int, deviceId: String)
+
+//TODO zipkin
+//TODO onComplete
+object Device {
+      implicit val deviceCodec: CodecJson[Device] =
+              casecodec2(Device.apply, Device.unapply)("id", "device_id")
+}
+
 // refs: https://twitter.github.io/finagle/guide/Quickstart.html
 object Server extends App {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
   val service = new Service[http.Request, http.Response] {
     def apply(req: http.Request): Future[http.Response] =
       Future.value(
         http.Response(req.version, http.Status.Ok)
       )
   }
-  val server = Http.serve(":8080", service)
+  //val server = Http.serve(":8080", service)
+  val config = ConfigFactory.load()
+  val databaseConfig = config.getConfig("database")
+  
+  val jdbcUrl = databaseConfig.getString("url")
+  val dbUser = databaseConfig.getString("user")
+  val dbPassword = databaseConfig.getString("password")
+
+  val db = Database.forURL(jdbcUrl, driver="com.mysql.jdbc.Driver", user=dbUser, password=dbPassword)
+      
+
+  val helloWorldApi: Endpoint[String] = get("hello") { Ok("Hello, World!") }
+  
+  //val device: Device = Device(1, "PP000000001")
+  val deviceApi: Endpoint[List[Device]] = get("device") { 
+      val query = sql"select id, device_id from device".as[(Int, String)]
+      val f: SFuture[Vector[(Int, String)]] = db.run(query)
+      var devices = List.empty[Device]
+      f onComplete {
+          case Success(rows) =>
+            for (row <- rows) {
+                devices = Device(row._1, row._2) :: devices
+            }
+
+          case Failure(_) =>
+              //do your staff
+            //BadRequest(throw new RuntimeException(""))
+      }
+      
+      Ok(devices)
+  }
+
+  val userService = (helloWorldApi :+: deviceApi).toService
+  val server = Http.server.serve(":8080", userService)
   Await.ready(server)
 }
