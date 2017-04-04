7cadaaf438fd8d1a664ec040800bf6ecdf54de34
letusfly85 <letusfly95@gmail.com>
2017-04-04 10:45:36 +0900

    add refference comment

--- src/main/scala/io/triplew/example/Server.scala
+++ src/main/scala/io/triplew/example/Server.scala
@@ -4,6 +4,7 @@ import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http
 import com.twitter.util.{Await, Future}
 
+// refs: https://twitter.github.io/finagle/guide/Quickstart.html
 object Server extends App {
   val service = new Service[http.Request, http.Response] {
     def apply(req: http.Request): Future[http.Response] =
