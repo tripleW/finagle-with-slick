a945b055bf1931d6fa928c331bc242c25d973a10
letusfly85 <letusfly95@gmail.com>
2017-04-05 00:35:58 +0900

    change row to case(id, ....)

--- src/main/scala/io/triplew/example/Server.scala
+++ src/main/scala/io/triplew/example/Server.scala
@@ -2,20 +2,17 @@ package io.triplew.example
 
 import argonaut.Argonaut._
 import argonaut.CodecJson
-import io.finch._
 import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http
 import com.twitter.util.{Await, Future}
 
-import scala.concurrent.{Future => SFuture, Await => SAwait}
+import scala.concurrent.{Await => SAwait, Future => SFuture}
 import scala.concurrent.duration._
-
 import com.typesafe.config.ConfigFactory
 import slick.driver.MySQLDriver.api._
 
 import io.finch._
 import io.finch.argonaut._
-import scala.util.{Failure, Success, Try}
 
 case class Device(id: Int, deviceId: String)
 
@@ -45,23 +42,19 @@ object Server extends App {
   val dbPassword = databaseConfig.getString("password")
 
   val db = Database.forURL(jdbcUrl, driver="com.mysql.jdbc.Driver", user=dbUser, password=dbPassword)
-      
 
   val helloWorldApi: Endpoint[String] = get("hello") { Ok("Hello, World!") }
   
   val deviceApi: Endpoint[List[Device]] = get("device") { 
-      val query = sql"select id, device_id from device".as[(Int, String)]
-      val f: SFuture[Vector[(Int, String)]] = db.run(query)
-      var devices = List.empty[Device]
-      val futureDevices = f.map {
-          case rows =>
-              for (row <- rows) {
-                devices = Device(row._1, row._2) :: devices
-              }
-      }
-      SAwait.result(futureDevices, Duration.Inf) 
-      
-      Ok(devices)
+    val query = sql"select id, device_id from device".as[(Int, String)]
+    val eventualVector: SFuture[Vector[(Int, String)]] = db.run(query.transactionally)
+
+    val futureDevices = eventualVector.map {f =>
+      f.map{ case(id, deviceId) => Device(id, deviceId)}.toList
+    }
+    val devices = SAwait.result(futureDevices, Duration.Inf)
+
+    Ok(devices)
   }
 
   val userService = (helloWorldApi :+: deviceApi).toService
